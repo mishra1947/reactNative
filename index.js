9aff74abc265cf01cce465a91db94f9dd872339e
shivam mishra <shivam.mishra@qandle.com>
2018-05-29 10:14:54 +0530

    album app done

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+// import a library to help create a component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
 
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+//create a  component
+const App = () => (
+    <View style={{ flex: 1 }}>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+    </View>
+);
+
+// render it to the device()
 AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/AlbumList.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+import { ScrollView } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './Albumdetail';
+
+class AlbumList extends Component {
+
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         albums: []
+    //     };
+    // } or
+
+    state = {
+        albums: []
+    }
+    
+    componentWillMount() {
+        // fetch('http://rallycoding.herokuapp.com/api/music_albums')
+        // .then((response) => response.json())
+        // .then((responseData) => {
+        //     console.log(responseData);
+        // });
+       axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+       .then((response) => {
+           this.setState({ albums: response.data });
+       });
+    }
+
+    renderAlbums() {
+        return this.state.albums.map((album) => 
+        <AlbumDetail key={album.title} album={album} />);
+    }
+
+    render() {
+        console.log(this.state);
+        return (
+            <ScrollView>
+                {this.renderAlbums()}
+            </ScrollView>
+        );
+    }
+}
+
+export default AlbumList;
